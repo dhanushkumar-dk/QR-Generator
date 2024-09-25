0da7ba51caf961ee652e4e97e3c4c55b6efdac28
Dhanush Kumar <112567261+dhanushkumar-dk@users.noreply.github.com>
2024-09-25 19:59:29 +0530

    Uploading frontend

---
+++ QR_Code_Generator/qr-backend/qr-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QRGenerator from './components/QRGenerator';
+
+
+function App() {
+  return (
+    <div className="App">
+      <QRGenerator />
+    </div>
+  );
+}
+
+export default App;
